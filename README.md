0f5e84b933ddd48ff06b0d6d76d3e6408c711abc
Niels Gerritsen <n.gerritsen@protonmail.com>
2023-12-02 13:57:19 +0100

    Day 02

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 My solutions for [Advent of Code 2023](https://adventofcode.com/2023) in [Typescript](https://www.typescriptlang.org/) (with [Bun](https://bun.sh/)).
 
+> I use the [Radash](https://radash-docs.vercel.app/docs/getting-started) library to avoid having to write common operations like `sum` all the time.
+
 ## Running the code
 
 > ⚠️ My input files are not committed [as per request](https://www.reddit.com/r/adventofcode/comments/zh2hk0/2022friendly_reminder_dont_commit_your_input/) of the Advent of Code author.
 
-1. Make sure [Bun](https://bun.sh/) is installed on the system.
+1. Make sure [Bun](https://bun.sh/) is installed on the system (macOS and Linux only).
 2. Add the input in a file named `input.txt` next to the solution before running.
 3. Run: `bun src/{day_number}/main.ts`
--- src/01/main.ts
+++ src/01/main.ts
@@ -1,3 +1,4 @@
+import { sum, isNumber, last, chain } from "radash";
 import input from "./input.txt";
 
 const NUMS = [
@@ -12,33 +13,21 @@ const NUMS = [
   "nine",
 ];
 
-function main() {
+const rx = new RegExp(NUMS.join("|"), "g");
+
+const main = () => {
   const lines = input.trim().split("\n");
 
-  console.log(lines.map(getNums).map(joinNums).reduce(sum, 0));
-  console.log(
-    lines.map(replaceNumbers).map(getNums).map(joinNums).reduce(sum, 0),
-  );
-}
+  console.log(sum(lines.map(chain(getNums, addNums))));
+  console.log(sum(lines.map(replaceNumbers).map(chain(getNums, addNums))));
+};
 
-function replaceNumbers(line: string) {
-  const chars = line.split("");
+const replaceNumbers = (line: string) =>
+  line
+    .replace(rx, (m) => String(NUMS.indexOf(m) + 1) + m.slice(1))
+    .replace(rx, (m) => String(NUMS.indexOf(m) + 1));
 
-  NUMS.forEach((word, index) => {
-    let position = line.indexOf(word);
-
-    while (position > -1) {
-      chars[position] = String(index + 1);
-      position = line.indexOf(word, position + 1);
-    }
-  });
-
-  return chars.join("");
-}
-
-const sum = (a: number, b: number) => a + b;
-const getNums = (line: string) => line.split("").filter(isNumber);
-const joinNums = (nums: string[]) => Number(nums[0] + nums[nums.length - 1]);
-const isNumber = (char: string) => !isNaN(Number(char));
+const getNums = (line: string) => line.split("").map(Number).filter(isNumber);
+const addNums = (nums: number[]) => nums[0] * 10 + (last(nums) as number);
 
 main();

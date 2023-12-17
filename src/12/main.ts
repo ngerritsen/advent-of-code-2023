7f298779148323df28034e3bebec8e7b12e85e65
Niels Gerritsen <n.gerritsen@protonmail.com>
2023-12-17 16:10:17 +0100

    Day 12 (Part 2)

--- src/12/main.ts
+++ src/12/main.ts
@@ -1,68 +1,54 @@
 import { sum } from "lodash";
-import input from "./example.txt";
+import input from "./input.txt";
 
 const main = () => {
   const rows = input.trim().split("\n").map(parseRow);
-  console.log(sum(rows.map((r) => getArrangements(r.springs, r.groups))));
+  console.log(sum(rows.map((r) => arrangements(r.springs, r.groups))));
+  console.log(sum(rows.map((r) => unfoldedArrangements(r.springs, r.groups))));
 };
 
-const getArrangements = (springs: string, groups: number[]) => {
-  const it = Array.from(Array(5));
-  springs = it.map(() => springs).join("?");
-  groups = it.flatMap(() => groups);
-  springs = springs
-    .replace(/^\.+/, "")
-    .replace(/\.+$/, "")
-    .replace(/\.+/g, ".");
+const cache: Record<string, number> = {};
 
-  let maxShift = springs.length - (sum(groups) + groups.length - 1);
-  const permutations = getPermutations(groups, maxShift);
+const unfoldedArrangements = (springs: string, groups: number[]) =>
+  arrangements(
+    getArr(5)
+      .map(() => springs)
+      .join("?"),
+    getArr(5).flatMap(() => groups),
+  );
 
-  const matches = permutations.filter((p) => {
-    for (let i = 0; i < springs.length; i++) {
-      if (!matchChar(springs[i], p[i])) {
-        return false;
-      }
-    }
-    return true;
-  });
+const arrangements = (springs: string, groups: number[]) => {
+  if (groups.length === 0) return springs.includes("#") ? 0 : 1;
 
-  return matches.length;
-};
+  const k = springs + groups.join(",");
 
-const matchChar = (sc: string, pc: string) => {
-  if (sc === "#") return pc === "#";
-  if (sc === ".") return pc !== "#";
-  if (sc === "?") return true;
-  return false;
-};
+  if (cache[k] !== undefined) return cache[k];
 
-const getPermutations = (
-  groups: number[],
-  maxShift: number,
-  curr = "",
-  gi = 0,
-  permutations: string[] = [],
-) => {
-  const g = "#".repeat(groups[gi]);
+  let n = 0;
 
-  for (let s = 0; s <= maxShift; s++) {
-    const springs = curr + ".".repeat(s + (curr ? 1 : 0)) + g;
-
-    if (gi === groups.length - 1) {
-      permutations.push(springs);
-      continue;
-    }
-
-    getPermutations(groups, maxShift - s, springs, gi + 1, permutations);
+  if (".?".includes(springs[0])) {
+    n += arrangements(springs.slice(1), groups);
   }
 
-  return permutations;
+  if ("#?".includes(springs[0])) {
+    const g = groups[0];
+    const r = springs.slice(0, g);
+
+    if (r.length === g && !r.includes(".") && springs[g] !== "#") {
+      n += arrangements(springs.slice(g + 1), groups.slice(1));
+    }
+  }
+
+  cache[k] = n;
+
+  return n;
 };
+
+const getArr = (length: number) => Array.from(Array(length));
 
 const parseRow = (row: string) => {
   const [springs, groups] = row.split(" ");
-  return { springs, groups: groups.split(",").map(Number) };
+  return { springs: springs, groups: groups.split(",").map(Number) };
 };
 
 main();
